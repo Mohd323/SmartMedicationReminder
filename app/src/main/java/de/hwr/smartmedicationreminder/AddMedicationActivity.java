113d79c98ee0383e57e624a2444661fddfcdfacf
aldarwish24 <s_aldarwish24@stud.hwr-berlin.de>
2026-07-12 18:08:44 +0200

    Implementiere die Zurück-Button-Funktionalität in der AddMedicationActivity

--- app/src/main/java/de/hwr/smartmedicationreminder/AddMedicationActivity.java
+++ app/src/main/java/de/hwr/smartmedicationreminder/AddMedicationActivity.java
@@ -1,6 +1,8 @@
 package de.hwr.smartmedicationreminder;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class AddMedicationActivity extends AppCompatActivity {
+
+    Button buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_medication);
+
+        // Zurück-Button finden
+        buttonBack = findViewById(R.id.buttonBack);
+
+        // Zur Home-Seite zurück
+        buttonBack.setOnClickListener(v -> {
+            Intent intent = new Intent(AddMedicationActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        });
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
